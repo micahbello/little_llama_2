a635250b35215a2bf8083d8a5ebbe494d31edae8
Miqueas Bello <micahbello@gmail.com>
2018-10-23 19:29:42 -0400

    Finished how to play modal

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 var path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './lib/little_llama.js',
   output: {
-    filename: './lib/bundle.js'
+    path: path.resolve(__dirname, "lib"),
+    filename: 'bundle.js'
   },
   devtool: 'source-map',
 }
